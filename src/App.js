7de46be4f68b97310b59491bcafcd9d054c6e807
janviig <83744327+janviig@users.noreply.github.com>
2024-02-20 00:10:15 +1100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css';
+import Page from './page';
+
+// export default function Form(){
+//   const [formData, setfromData] = useState({
+//     type : ""
+//   })
+// }
+
+function App() {
+  return (
+    <div>
+      <Page />
+      <div>
+    
+      </div>
+    </div>
+  );
+}
+
+export default App;
